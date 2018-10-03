4d87b6e299ea9975621cfd037602c8797e634446
ruijue lu <jenson.lrj@gmail.com>
2018-10-03 09:41:07 +0000

    #Removed scripts written for test against attempts and random_numbers, #Updated README.md's content

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Techdegree-Project-1
+
+INTRODUCTION
+
+GuessingGame is a simple game written in python programming languages. The program covers python programming fundamentals: variable declaration and manipulation (strings and
+integers), screen output, keyboard input, loops, if statements, try & execeptions, random function and time function.
+
+EXAMPLE
+
+Below is an example of playing the guessing game. The game welcome the player and provides instruction on the objectives of the game. The best score(lowest attempts/tries) is displayed prior to the commencement of the game. The program randomly picks a number which you try to guess.. The best score  was created to exist for as long as the game/script runs. When a player ends the game and relaunches the script, the high score will be reset again. 
+
+    ////////////////////////////////////////////                                                 
+    /////// WELCOME TO THE GUESSING GAME ///////                                                 
+    ////////////////////////////////////////////                                                 
+    /////////////// INSTRUCTIONS: //////////////                                                 
+    // Guess a random number between 1 to 99 ///                                                 
+    // Lesser the attempts the better you are //                                                 
+    ////////////////////////////////////////////                                                 
+                                                                                                 
+Best score so far: 4 Attempt(s)                                                                  
+Press 'ENTER' to start the game                                                                  
+LET THE GAME BEGINS!                                                                             
+Please enter a numerical value between 0 to 99: 45                                               
+It's Too Low                                                                                     
+Please enter a numerical value between 0 to 99: 75                                               
+It's Too High                                                                                    
+Please enter a numerical value between 0 to 99: 63                                               
+It's Too Low                       
+Please enter a numerical value between 0 to 99: 72                                               
+It's Too Low                                                                                     
+Please enter a numerical value between 0 to 99: 76                                               
+It's Too High                                                                                    
+Please enter a numerical value between 0 to 99: 75                                               
+It's Too High                                                                                    
+Please enter a numerical value between 0 to 99: 74                                               
+CONGRATULATIONS!! 74 is the CORRECT Guess!!                                                      
+Your total attempts: 8                                                                           
+Press 'Y' if you like to play again. If not press ANY key to end the game: n                     
+                                                                                                 
+                /////////////////////                                                            
+                //// !GAME OVER! ////                                                            
+                /////////////////////                                                            
+                                                                                             
+
+LINKS
+
+https://github.com/ruijuelu/Techdegree-Project-1.git
+        
--- guessing_game.py
+++ guessing_game.py
@@ -1,10 +1,31 @@
-import random 
+import random
+import time
 
 def start_game(best_score = 6):
-    print("~~Welcome to the Guessing Game!!~~\n **Best score so far: {} Attempts**".format(best_score))  
+    print("""
+    ////////////////////////////////////////////
+    /////// WELCOME TO THE GUESSING GAME ///////
+    ////////////////////////////////////////////
+    /////////////// INSTRUCTIONS: //////////////
+    // Guess a random number between 1 to 99 ///
+    // Lesser the attempts the better you are //
+    ////////////////////////////////////////////
+    """)
     
-    name = input("Please enter your name: ")
-    print("Hi {}, let the game begins!".format(name))
+    print("Best score so far: {} Attempt(s)".format(best_score))  
+    time.sleep(2) #capture the attention of player to read the game instructions & best score.
+    
+    while True:
+        commence_game = input("Press 'ENTER' to start the game ")
+        try: #ensure that player only key 'enter' to commence the game
+            if commence_game != '':
+                raise ValueError #print statement not require, as input instruction is repetitive
+            break
+        except ValueError:
+            continue
+
+    time.sleep(1) #provide a brief waiting time for player before the next set of instructions 
+    print("LET THE GAME BEGINS!")
     #https://docs.python.org/2/library/random.html
     random_number = random.randrange(1,99)  
     
@@ -18,55 +39,70 @@ def start_game(best_score = 6):
             if answer == str:
                 raise ValueError
         except ValueError:
-            continue
+            print("Invalid entry, Please try again..")
+            continue 
     
         try: #exception raise to prevent player from entering a number smaller than 0 or bigger than 99
             if answer <= 0 or answer >= 100:
                 raise ValueError
         except ValueError:
-            print(random_number)
             print("Invalid entry, Please try again..")
             continue
         
         attempts += 1 #attempts of += 1 placed below both the exceptions, as exception should not be counted as an attempt
-        print(attempts) #####remove it @ final check before FINAL SUBMISSION!!!####
         too_low = answer < random_number
         too_high = answer > random_number
     
         if answer == random_number:
-            print("CONGRATULATIONS {}!! {} is the CORRECT Guess!!".format(name, answer))
+            time.sleep(1.5)
+            print("CONGRATULATIONS!! {} is the CORRECT Guess!!".format(answer))
             print("Your total attempts: {}".format(attempts))
             
             if attempts < best_score: #IF loop applied to update lastest best_score
                 best_score = attempts 
-                print("WOW!! You SET A NEW Record!!")
+                print("""
+                *****************************************
+                *** WOW!! YOU HAVE SET A NEW RECORD!! ***
+                *****************************************
+                """)
 #googled tips on https://stackoverflow.com/questions/35873400/python-guessing-game explained by Developer: eskaev                
             else:
                 best_score = min(attempts, best_score)
         
-            question = input("Press 'Y' if you like to play again. Press any key to end the game: ") 
+            question = input("Press 'Y' if you like to play again. If not press ANY key to end the game: ") 
             
             if question.upper() == "Y":
-                print("////Game Resetting////")
+                print("""
+                //////////////////////////////
+                //// ...GAME RESETTING... ////
+                //////////////////////////////
+                """)
+                
+                time.sleep(3) #create a pause effect to signify a start of a new game session
                     
                 start_game(best_score)
                 
             else:
-                print("////GAME OVER!////") 
+                time.sleep(2) #create an effect to suggest closure, set at 2 sec as to need to create anticipation (player is discontinuing the game) 
+                print("""
+                /////////////////////
+                //// !GAME OVER! ////
+                /////////////////////
+                """) 
                 break #end the nested IF loop
                    
             break #end the WHILE loop
         
         elif too_low:
-            print(random_number)#####remove it @ final check before FINAL SUBMISSION!!!####
+            time.sleep(1.5)#creates the effect of anticipation
             print("It's Too Low")
             continue
             
         elif too_high:
-            print(random_number)#####remove it @ final check before FINAL SUBMISSION!!!####
+            time.sleep(1.5) #creates the effect of anticipation
             print("It's Too High")
             continue
             
 if __name__ == '__main__':
     # Kick off the program by calling the start_game function.        
-    start_game() 
+    start_game()
