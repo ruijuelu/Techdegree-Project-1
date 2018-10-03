b0ef1bda0f1b0527965a37ce3c9e584aedbc11da
ruijue lu <jenson.lrj@gmail.com>
2018-10-03 04:36:39 +0000

    #Added auto best score update, #Raise ValueError & exceptions, #Added messages to indicate start, end and reset of a game session.

--- guessing_game.py
+++ guessing_game.py
@@ -1,27 +1,72 @@
 import random 
 
-def start_game():
-    name = input("Welcome~ Please enter your name: ")
+def start_game(best_score = 6):
+    print("~~Welcome to the Guessing Game!!~~\n **Best score so far: {} Attempts**".format(best_score))  
+    
+    name = input("Please enter your name: ")
     print("Hi {}, let the game begins!".format(name))
     #https://docs.python.org/2/library/random.html
-    random_number = random.randrange(1,99) 
-
+    random_number = random.randrange(1,99)  
+    
+    attempts = 0 #number of tries starts from 0
+    
     while True:
-        answer = input("Please enter a number: ")
-        answer = int(answer)
+        
+        try: #exception raise to prevent player from entering a string
+            answer = input("Please enter a numerical value between 0 to 99: ")
+            answer = int(answer)
+            if answer == str:
+                raise ValueError
+        except ValueError:
+            continue
+    
+        try: #exception raise to prevent player from entering a number smaller than 0 or bigger than 99
+            if answer <= 0 or answer >= 100:
+                raise ValueError
+        except ValueError:
+            print(random_number)
+            print("Invalid entry, Please try again..")
+            continue
+        
+        attempts += 1 #attempts of += 1 placed below both the exceptions, as exception should not be counted as an attempt
+        print(attempts) #####remove it @ final check before FINAL SUBMISSION!!!####
         too_low = answer < random_number
         too_high = answer > random_number
     
         if answer == random_number:
             print("CONGRATULATIONS {}!! {} is the CORRECT Guess!!".format(name, answer))
-            break
+            print("Your total attempts: {}".format(attempts))
+            
+            if attempts < best_score: #IF loop applied to update lastest best_score
+                best_score = attempts 
+                print("WOW!! You SET A NEW Record!!")
+#googled tips on https://stackoverflow.com/questions/35873400/python-guessing-game explained by Developer: eskaev                
+            else:
+                best_score = min(attempts, best_score)
+        
+            question = input("Press 'Y' if you like to play again. Press any key to end the game: ") 
+            
+            if question.upper() == "Y":
+                print("////Game Resetting////")
+                    
+                start_game(best_score)
+                
+            else:
+                print("////GAME OVER!////") 
+                break #end the nested IF loop
+                   
+            break #end the WHILE loop
         
         elif too_low:
-            print("Too Low")
+            print(random_number)#####remove it @ final check before FINAL SUBMISSION!!!####
+            print("It's Too Low")
             continue
             
         elif too_high:
-            print("Too High")
+            print(random_number)#####remove it @ final check before FINAL SUBMISSION!!!####
+            print("It's Too High")
             continue
-        
-start_game() 
+            
+if __name__ == '__main__':
+    # Kick off the program by calling the start_game function.        
+    start_game() 
